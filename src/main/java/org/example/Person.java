a15f4e0969e539297dfe19874e10f62ec3e83eea
eceykilic <eceykilic@gmail.com>
2024-03-20 12:57:41 +0300

    solutions

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    String meslek;
+    String cinsiyet;
+    int favoriSayi;
+
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this(firstName, lastName);
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age, String meslek, String cinsiyet, int favoriSayi) {
+        this(firstName, lastName, age);
+        this.meslek = meslek;
+        this.cinsiyet = cinsiyet;
+        this.favoriSayi = favoriSayi;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+}
